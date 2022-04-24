928e4f41d4287823fdebcff0adad9c20f3f256ba
EgorStrupinskiy <venik2405@yandex.ru>
2022-04-24 14:02:28 +0300

    good design, everything works

--- script.js
+++ script.js
@@ -1,23 +1,81 @@
-let searchButton = document.querySelector("#search")
-
-//Add an event listener to the button that runs the function sendApiRequest when it is clicked
-searchButton.addEventListener("click", () => {
-    console.log("button pressed")
-    sendApiRequest()
+document.addEventListener("DOMContentLoaded", () => {
+    const data = localStorage.getItem("data")
+    const date = localStorage.getItem("date")
+    if (data && date) {
+        updateData(JSON.parse(data))
+        setDate(date)
+    } else {
+        setDate(Date.now())
+        loadData(document.getElementById("date-input").value)
+    }
 })
 
-//An asynchronous function to fetch data from the API.
-async function sendApiRequest() {
-    let API_KEY = 'd1vY2IBmEMD0DVprM9Eg23OxSRczuNTpM54Aqnjl'
-    let response = await fetch(`https://api.nasa.gov/planetary/apod?date=2022-04-22&api_key=${API_KEY}`);
-    console.log(response)
-    let data = await response.json()
-    console.log(data)
-    useApiData(data)
+document.getElementById("search-form").addEventListener("submit", () => {
+    document.getElementById("content-loader").style.display = "flex"
+    document.getElementById("content").style.display = "none"
+    document.getElementById("content-error").style.display = "none"
+    const date = document.getElementById("date-input").value
+    loadData(date)
+    setDate(date)
+})
+
+const updateData = (data) => {
+    let elem
+    if (data.code == 400) {
+        document.getElementById("content-loader").style.display = "none"
+        document.getElementById("content-error").style.display = "flex"
+    } else {
+        switch (data.media_type) {
+            case "image":
+                elem = document.getElementById("api__image")
+                document.getElementById("api__video").classList.remove("active")
+                elem.attributes.src.value = data.url
+                elem.classList.add("active")
+                break;
+            case "video":
+                elem = document.getElementById("api__video")
+                document.getElementById("api__image").classList.remove("active")
+                elem.attributes.src.value = data.url
+                elem.classList.add("active")
+                break;
+        }
+        document.getElementById("api__title").innerText = data.title
+        document.getElementById("api__description").innerText = data.explanation
+        document.getElementById("content-loader").style.display = "none"
+        document.getElementById("content").style.display = "flex"
+    }
 }
 
-//function that does something with the data received from the API. The name of the function should be customized to whatever you are doing with the data
-function useApiData(data) {
-    document.querySelector("#api__text").innerHTML = data.explanation;
-    document.querySelector("#api__image").innerHTML = `<img src = "${data.url}">`
+const loadData = (date) => {
+    document.getElementById("content-connection-error").style.display = "none"
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=d1vY2IBmEMD0DVprM9Eg23OxSRczuNTpM54Aqnjl&date=${date}`)
+        .then((response) => {
+            return response.json()
+        })
+        .catch(() => {
+            offlineHandler()
+        })
+        .then((data) => {
+            if (data) {
+                localStorage.setItem("data", JSON.stringify(data))
+                localStorage.setItem("date", document.getElementById("date-input").value)
+                updateData(data)
+            }
+        })
 }
+
+const setDate = (date) => {
+    const d = new Date(date)
+    document.getElementById("date-input").valueAsDate = d
+    document.getElementById("date").innerHTML = d.toLocaleString("ru", {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric"
+    })
+}
+
+const offlineHandler = () => {
+    document.getElementById("content-loader").style.display = "none"
+    document.getElementById("content-connection-error").style.display = "flex"
+}
+
